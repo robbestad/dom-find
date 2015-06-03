0afdd8aaf3df4603b6e577e1e4e6a1c606cfe6c1
Sven Anders Robbestad <robbestad@gmail.com>
2015-06-03 13:48:29 +0200

    0.0.6

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 // findPos() by quirksmode.org
 // Finds the absolute position of an element on a page
-var findPos = require("./findpos");
-var getPageScroll = require("./getpagescroll");
-var findPosRelativeToViewport = require("./findpostrelativetoviewport");
-
-exports.findPos = findPos();
-exports.getPageScroll = getPageScroll();
-exports.findPosRelativeToViewport = findPosRelativeToViewport();
+var findPos = require("./findPos");
+exports.findPos = findPos;
+var getPageScroll = require("./getPageScroll");
+exports.getPageScroll = getPageScroll;
+var findPosRelativeToViewport = require("./findPosRelativeToViewport");
+exports.findPosRelativeToViewport = findPosRelativeToViewport;
 
